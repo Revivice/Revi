caaebedf7c8d01334526b464dd96f25518f3a28b
Kabuto <tju_ren@git.com>
2021-10-28 16:05:50 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("what are you doing");
         System.out.println("I am doing nothing");
+        System.out.println("wow, you can really dance");
     }
 }
