a53ae286c01adfad0549a19f45775caa7d1b1541
Revivice <93263460+Revivice@users.noreply.github.com>
2021-10-28 16:02:26 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("what are you doing");
+        System.out.println("I am doing nothing");
     }
 }
